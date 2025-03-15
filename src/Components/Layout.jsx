c4fbfc39f213c16e2e9d40919690facb44b5b9a1
nitin17-01 <nitinmaurya0704@gmail.com>
2025-03-15 14:32:53 +0530

    removed my name

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -10,14 +10,14 @@ const Layout = () => {
       <footer className="w-full text-center py-6 mt-8 text-white bg-gray-800">
         <p>&copy; 2025 Vivek Patel. All rights reserved.</p>
         <p>
-          <a
+          {/* <a
             href="https://portfolio-indol-ten-94.vercel.app/"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:underline text-sm tracking-tight text-gray-400"
           >
             Designed & Developed by Nitin Maurya.
-          </a>
+          </a> */}
         </p>
       </footer>
     </div>
